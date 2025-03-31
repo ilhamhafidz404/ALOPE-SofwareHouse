e7fc57d99c3c2f1b8034b77a798df66bc29673e3
ilhamhafidz404 <xxspanzie@gmail.com>
2025-04-01 03:47:59 +0700

    Lets Build Something Awesome

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-3xl font-bold underline">
+        Lets Build Something Awesome!
+      </h1>
+    </div>
+  );
+}
